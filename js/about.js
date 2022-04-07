3951ce42e9c90939877e16715459be420b5b067a
farima4 <jovozivanovic1995@gmail.com>
2022-04-07 20:38:57 +0200

    about page updated

--- js/about.js
+++ js/about.js
@@ -1,7 +1,10 @@
 const srpski = 'Websajt napravio Marko Kovacevic koristeci html, css i javascript. Poenta je bila nauciti kako se prave webstranice. Ovdje se mogu naci neki programi koje sam napravio ili se instalirati igrice koje sam napravio sa python-om.';
-
 const engleski = 'Website was made by Marko Kovacevic using html, css and javascript. The goal was to learn how to make webpages. Here you can find some programs I made or you can install games I made with python.';
 
+let dateeng = 'April 7th 2022';
+let dateserb = 'sedmi april 2022';
+
+document.getElementById('par6').innerHTML = 'websajt azuriran: ' + dateeng;
 let indicator = 'srpski';
 
 document.getElementById('code').onclick = function() {
@@ -10,6 +13,9 @@ document.getElementById('code').onclick = function() {
         document.getElementById('par2').innerHTML = 'Source code is available on GitHub.';
         document.getElementById('par3').innerHTML = 'Hosted for free on ';
         document.getElementById('par4').innerHTML = 'Creation of the website began on: April 5th 2022';
+        document.getElementById('par5').innerHTML = 'Website went online: April 7th 2022';
+        document.getElementById('par6').innerHTML = 'Website last updated: ' + dateeng;
+
         indicator = 'engleski';
     }
     else {
@@ -17,6 +23,8 @@ document.getElementById('code').onclick = function() {
         document.getElementById('par2').innerHTML = 'Source code na git hub-u';
         document.getElementById('par3').innerHTML = 'Besplatno hostovano pomocu ';
         document.getElementById('par4').innerHTML = 'Izrada webstranice zapocela: petog aprila 2022';
+        document.getElementById('par5').innerHTML = 'websajt postavljen onlajn: sedmog aprila 2022';
+        document.getElementById('par6').innerHTML = 'websajt azuriran: ' + dateserb;
         indicator = 'srpski';
     }
 }
